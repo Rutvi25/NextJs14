92ef3538d3077fc2b6e16f634da3e786e1491e11
Rutvi <rutvi.p@simformsolutions.com>
2024-02-20 23:15:08 +0530

    not found page

---
+++ next14-learning/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h2>Page not found</h2>
+      <p>Couldn't found requested resource</p>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ next14-learning/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h2>Review not found</h2>
+      <p>Couldn't found requested resource</p>
+    </div>
+  );
+};
+
+export default NotFound;
