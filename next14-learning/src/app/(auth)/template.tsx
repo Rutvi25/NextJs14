04893a40779e0e20f34030e020ebc9ce33fa7e2a
Rutvi <rutvi.p@simformsolutions.com>
2024-02-21 23:45:53 +0530

    templates

--- next14-learning/src/app/(auth)/template.tsx
+++ next14-learning/src/app/(auth)/template.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import './style.css';
+import { useState } from 'react';
 
 const navLinks = [
   { name: 'Register', href: '/register' },
@@ -12,8 +13,12 @@ const navLinks = [
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
+  const [input, setInput] = useState('');
   return (
     <div>
+      <div>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
       {navLinks.map((link) => {
         const isActive = pathname.startsWith(link.href);
         return (
