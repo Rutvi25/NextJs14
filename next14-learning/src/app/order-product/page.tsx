da9e090437a7cdb39477e1e6c8e1584f5ea60d08
Rutvi <rutvi.p@simformsolutions.com>
2024-02-21 23:34:27 +0530

    navigating programmatically

---
+++ next14-learning/src/app/order-product/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+const OrderProduct = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('Placing the order...');
+    router.push('/');
+  };
+
+  return (
+    <>
+      <h1>Order Product</h1>
+      <button onClick={handleClick}>Place order</button>
+    </>
+  );
+};
+
+export default OrderProduct;
