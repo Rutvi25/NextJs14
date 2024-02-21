cd6ed0b2873a3b8286e708d94fc9cc526fb943c0
Rutvi <rutvi.p@simformsolutions.com>
2024-02-21 22:47:24 +0530

    routing metadata

--- next14-learning/src/app/products/[productId]/page.tsx
+++ next14-learning/src/app/products/[productId]/page.tsx
@@ -1,4 +1,26 @@
-const ProductDetails = ({ params }: { params: { productId: string } }) => {
+import { Metadata } from 'next';
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+
+  return {
+    title: `Products ${title}`,
+  };
+};
+
+const ProductDetails = ({ params }: Props) => {
   return <h1>Details for Product {params.productId}</h1>;
 };
 
